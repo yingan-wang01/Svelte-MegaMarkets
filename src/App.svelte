f4236ead98bca61331d0a9ab843ae9bc76615c98
Ying-An <yingan.wang@berkeley.edu>
2023-05-28 12:43:31 -0700

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
